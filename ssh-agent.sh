df76e79bf01ee7ad9dd09a7c6a928ad48373b16b
Pål Sollie <sollie@sparkz.no>
2021-06-21 18:22:57 +0200

    debug

--- ssh-agent.sh
+++ ssh-agent.sh
@@ -5,6 +5,6 @@ mkdir -p /home/runner/.ssh
 ssh-keyscan $INPUT_GHES_HOST >> /home/runner/.ssh/known_hosts
 echo "$INPUT_SSH_PRIVATE_KEY" > /home/runner/.ssh/github_actions
 chmod 600 /home/runner/.ssh/github_actions
-ssh-agent -a $INPUT_SSH_AUTH_SOCK> /dev/null
+ssh-agent -a $INPUT_SSH_AUTH_SOCK > /dev/null
 ssh-add /home/runner/.ssh/github_actions
-echo SSH_AUTH_SOCK=$INPUT_SSH_AUTH_SOCK >> $GITUB_ENV
+echo "SSH_AUTH_SOCK=$INPUT_SSH_AUTH_SOCK" >> $GITUB_ENV
