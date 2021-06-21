0f9667a2db922b2a14608fa473fd4b5ec6b72d3a
Pål Sollie <sollie@sparkz.no>
2021-06-21 18:33:48 +0200

    Rename script, add git config

--- action.sh
+++ action.sh
@@ -7,3 +7,5 @@ echo "$INPUT_SSH_PRIVATE_KEY" > /home/runner/.ssh/github_actions
 chmod 600 /home/runner/.ssh/github_actions
 ssh-agent -a $INPUT_SSH_AUTH_SOCK > /dev/null
 ssh-add /home/runner/.ssh/github_actions
+
+git config --global --add url."git@$INPUT_GHES_HOST:".insteadOf "https://$INPUT_GHES_HOST/"
