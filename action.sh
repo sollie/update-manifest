d6a3cc3b5793e85ad8a199b77831333c43e930af
Pål Sollie <sollie@sparkz.no>
2021-08-04 12:43:40 +0200

    Quote image tag

--- action.sh
+++ action.sh
@@ -17,7 +17,7 @@ git clone git@${INPUT_GHES_HOST}:${INPUT_MANIFEST_ORG}/${INPUT_MANIFEST_REPO}.gi
 
 cd ${INPUT_MANIFEST_REPO}/${INPUT_MANIFEST_PATH}/${INPUT_MANIFEST_NAME}
 OLD_IMAGE=$(cat kustomization.yaml | grep -A3 ${INPUT_MANIFEST_NAME} | grep newTag | awk '{print $2}')
-NEW_IMAGE=${INPUT_IMAGE_TAG}
+NEW_IMAGE="\"${INPUT_IMAGE_TAG}\""
 sed -i "s|${OLD_IMAGE}|${NEW_IMAGE}|g" kustomization.yaml
 git commit -m "Bump image - ${INPUT_MANIFEST_NAME}:${INPUT_IMAGE_TAG}" kustomization.yaml
 git push --force
